bef560f1b271336bdb78935f570196725c989b85
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-06-24 17:29:53 -0500

    minor updates

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,8 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-import Header from "./Header";
+// import Header from "./Header";
 import "./App.css";
 
 function App() {
-  return <Header />;
+  return <div className="app">Hello</div>;
 }
 
 export default App;
