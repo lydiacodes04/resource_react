77195224fa4905f4dec97df0490a345f127bb225
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-06-24 16:43:51 -0500

    developing file structure

---
+++ vite-project/src/components/App/Header.jsx
@@ -0,0 +1,17 @@
+// Header.jsx
+import "./Header.css";
+import reactLogo from "./assets/react.svg";
+
+function Header() {
+  return (
+    // Logo
+    <reactLogo />
+    // Generate current date using JS
+    // const currentDate = new Date().toLocaleString('default', { month: 'long', day: 'numeric' });
+    // Current location
+    //addClothes btn that opens ModalWithForm
+    //user's name and avatar
+  );
+}
+
+export default Header;
