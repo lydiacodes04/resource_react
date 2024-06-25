3d931cd4349793bd878e0f04b3301ccdafd3c13b
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-06-25 17:59:41 -0500

    get ItemCards displaying

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,16 +1,19 @@
 // ItemCard.jsx
 import "./ItemCard.css";
 
-function ItemCard() {
+function ItemCard({ item }) {
   return (
-    <> </>
-    //ItemCard is a component that renders the image and title for an item of clothing.
-    // the image is an interactive element, meaning that if the user clicks on it, the item modal will open
-    //Note that the item card itself doesn’t know about the modal state
-    // Therefore, you need to pass it down from App to Main
-    // In other words, when the user clicks on the image,
-    //you need to call the state change function handleCardClick() that ItemCard receives as a prop.
+    <li className="card">
+      <h2 className="card__name">{item.name}</h2>
+      <img className="card__image" src={item.link} alt={item.name} />
+    </li>
   );
 }
+//ItemCard is a component that renders the image and title for an item of clothing.
+// the image is an interactive element, meaning that if the user clicks on it, the item modal will open
+//Note that the item card itself doesn’t know about the modal state
+// Therefore, you need to pass it down from App to Main
+// In other words, when the user clicks on the image,
+//you need to call the state change function handleCardClick() that ItemCard receives as a prop.
 
 export default ItemCard;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,10 @@
 // Main.jsx
 import "./Main.css";
 import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
 
-function Main() {
+function Main({ weatherData }) {
   return (
     <main>
       <WeatherCard />
@@ -10,7 +12,15 @@ function Main() {
         <p className="cards__text">
           Today is 75 &deg; F / You may want to wear:
         </p>
-        {/* TO DO: ADD THE CARDS */}
+        <ul className="cards__list">
+          {defaultClothingItems
+            // .filter((item) => {
+            //   return item.weather === weatherData.type;
+            // })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
+        </ul>
       </section>
     </main>
   );
