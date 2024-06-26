57969bdf5598fc35f48a0d7659b38bca05f3da7e
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-06-26 12:55:59 -0500

    begin refactoring ModalWithForm

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,7 +1,7 @@
 //ModalWithForm.jsx
 import "./ModalWithForm.css";
 
-function ModalWithForm() {
+function ModalWithForm({ children }) {
   return (
     <div className="modal">
       <div className="modal__content">
@@ -10,48 +10,7 @@ function ModalWithForm() {
           CLOSE
         </button>
         <form className="modal__form">
-          <label htmlFor="" className="modal__label">
-            Name{" "}
-            <input
-              type="text"
-              className="modal__input"
-              id="name"
-              placeholder="Name"
-            />
-          </label>
-          <label htmlFor="imageUrl" className="modal__label">
-            Image{" "}
-            <input
-              type="text"
-              className="modal__input"
-              id="imageUrl"
-              placeholder="Image URL"
-            />
-          </label>
-          <fieldset className="modal__radio-buttons">
-            <legend className="modal__legend">Select the weather type: </legend>
-            <label
-              htmlFor="hot"
-              className="modal__label modal__label_type_radio"
-            >
-              <input id="hot" type="radio" className="modal__radio-input" />
-              Hot
-            </label>
-            <label
-              htmlFor="warm"
-              className="modal__label modal__label_type_radio"
-            >
-              <input id="warm" type="radio" className="modal__radio-input" />{" "}
-              Warm
-            </label>
-            <label
-              htmlFor="cold"
-              className="modal__label modal__label_type_radio"
-            >
-              <input id="cold" type="radio" className="modal__radio-input" />{" "}
-              Cold
-            </label>
-          </fieldset>
+          {children}
           <button type="submit" className="modal__submit">
             Add garment
           </button>
