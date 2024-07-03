11ac7a4586ba6fd267a2f2fa0a7a9c208d509342
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-07-02 21:00:48 -0500

    continue to work on setting up api requests

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -38,8 +38,8 @@ export const defaultClothingItems = [
 ];
 
 export const coordinates = {
-  latitude: 29.424122;
-  longitude: -98.493629;
-}
+  latitude: 29.424122,
+  longitude: -98.493629,
+};
 export const APIkey = "a562052a9800bf432ffc60528926e77f";
 // export const apiKey = a562052a9800bf432ffc60528926e77f;
