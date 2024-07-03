d734b0b5fdd2fe1598234e2f87fb9390e2a02358
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-07-03 09:44:45 -0500

    fix bug in radio buttons

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -74,20 +74,37 @@ function App() {
         <fieldset className="modal__radio-buttons">
           <legend className="modal__legend">Select the weather type: </legend>
           <label htmlFor="hot" className="modal__label modal__label_type_radio">
-            <input id="hot" type="radio" className="modal__radio-input" />
+            <input
+              id="hot"
+              type="radio"
+              className="modal__radio-input"
+              name="input"
+            />
             Hot
           </label>
           <label
             htmlFor="warm"
             className="modal__label modal__label_type_radio"
           >
-            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+            <input
+              id="warm"
+              type="radio"
+              className="modal__radio-input"
+              name="input"
+            />{" "}
+            Warm
           </label>
           <label
             htmlFor="cold"
             className="modal__label modal__label_type_radio"
           >
-            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+            <input
+              id="cold"
+              type="radio"
+              className="modal__radio-input"
+              name="input"
+            />{" "}
+            Cold
           </label>
         </fieldset>
       </ModalWithForm>
