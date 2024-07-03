9afe1368cef6985a11f1604b57986d19072f5183
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-07-03 09:46:09 -0500

    correct footer tag

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,12 +2,12 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="footer">
+    <footer className="footer">
       <ul className="footer__content">
         <li className="footer__content-name">Designed by Lydia Lockhart </li>
         <li className="footer__content-year"> 2024</li>
       </ul>
-    </div>
+    </footer>
   );
 }
 
