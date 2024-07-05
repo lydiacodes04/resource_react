1dcf0a70f43d25eda8a90babd47504970de96da2
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-07-05 14:16:02 -0500

    finish styling item modal

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,11 +1,12 @@
 import "./ItemModal.css";
+import close from "../../assets/close_icon-white.svg";
 
 function ItemModal({ activeModal, onClose, card }) {
   return (
     <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
       <div className="modal__content modal__content_type_image">
         <button onClick={onClose} type="button" className="modal__close">
-          CLOSE
+          <img src={close} alt="close button" className="modal__close-btn" />
         </button>
         <img src={card.link} alt={card.name} className="modal__image" />
         <div className="modal__footer">
