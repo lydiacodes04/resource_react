b4691713673ea9cb6d232ecd82221270fbc51878
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-07-13 18:13:17 -0500

    get Toggle Switch working

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -2,10 +2,39 @@ import React from "react";
 import "./ToggleSwitch.css";
 
 const ToggleSwitch = () => {
-  console.log("Toggle");
+  const [CurrentTemperatureUnit, handleToggleSwitchChange] =
+    React.useState("C");
+  const handleChange = (e) => {
+    if (CurrentTemperatureUnit === "C") handleToggleSwitchChange("F");
+    if (CurrentTemperatureUnit === "F") handleToggleSwitchChange("C");
+  };
+  console.log(CurrentTemperatureUnit);
+
   return (
     <label className="switch">
-      <input type="checkbox" className="switch__box" />
+      <input type="checkbox" className="switch__box" onChange={handleChange} />
+      <span
+        className={
+          CurrentTemperatureUnit === "F"
+            ? "switch__slider switch__slider-F"
+            : "switch__slider switch__slider-C"
+        }
+      ></span>
+
+      <p
+        className={`switch__temp-F ${
+          CurrentTemperatureUnit === "F" && "switch__active"
+        }`}
+      >
+        F
+      </p>
+      <p
+        className={`switch__temp-C ${
+          CurrentTemperatureUnit === "C" && "switch__active"
+        }`}
+      >
+        C
+      </p>
     </label>
   );
 };
