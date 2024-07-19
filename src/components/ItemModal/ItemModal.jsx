6b9939bdf0c0f8796a942fab40323f041c284355
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-07-19 17:57:42 -0500

    create delete item button

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -12,6 +12,7 @@ function ItemModal({ activeModal, onClose, card }) {
         <div className="modal__footer">
           <h2 className="modal__caption"> {card.name} </h2>
           <p className="modal__weather">Weather: {card.weather}</p>
+          <button className="modal__delete-btn">Delete item</button>
         </div>
       </div>
     </div>
