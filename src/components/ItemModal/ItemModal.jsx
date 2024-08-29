044a71bd972c8eaf23a38699acf999f45f853da5
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-08-29 16:41:24 -0500

    update ItemModal with classes to determine visibility

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -2,8 +2,18 @@ import "./ItemModal.css";
 import close from "../../assets/close_icon-white.svg";
 
 function ItemModal({ activeModal, onClose, card, handleDeleteItem }) {
+  // Checking if the current user is the owner of the current clothing item
+  const isOwn = selectedCard.owner === currentUser._id;
+
+  // Creating a variable which you'll then set in `className` for the delete button
+  const itemDeleteButtonClassName = `item__delete-button ${
+    isOwn ? "item__delete-button_visible" : "item__delete-button_hidden"
+  }`;
+
   return (
-    <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
+    <div
+      className={`modal ${activeModal === "preview" && "modal_opened"} isOwn`}
+    >
       <div className="modal__content modal__content_type_image">
         <button onClick={onClose} type="button" className="modal__close">
           <img src={close} alt="close button" className="modal__close-btn" />
@@ -13,7 +23,7 @@ function ItemModal({ activeModal, onClose, card, handleDeleteItem }) {
           <h2 className="modal__caption"> {card.name} </h2>
           <p className="modal__weather">Weather: {card.weather}</p>
           <button
-            className="modal__delete-btn"
+            className="modal__delete-btn, itemDeleteButtonClassName"
             onClick={() => handleDeleteItem(card) && onClose}
           >
             Delete item
