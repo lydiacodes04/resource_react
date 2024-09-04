9f197aea45f36383783aafc72461b17bf2cf03e2
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-09-04 13:02:28 -0500

    create card heart for likes and functionality

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -13,10 +13,15 @@ function ItemCard({ item, onCardClick, onCardLike }) {
   return (
     <li className="card">
       <h2 className="card__name">{item.name}</h2>
-      <img className="card__heart">{cardHeart}</img>
       <img
-        onClick={handleCardClick}
+        className="card__heart"
+        onClick={handleLike}
+        src={cardHeart}
+        alt="card like button"
+      />
+      <img
         className="card__image"
+        onClick={handleCardClick}
         src={item.imageUrl}
         alt={item.name}
       />
