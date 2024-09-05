d55de97909e3102eb0dcd6f056ea692c63054091
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-09-05 11:20:11 -0500

    develop cardHeartClassName

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -10,12 +10,22 @@ function ItemCard({ item, onCardClick, onCardLike }) {
     onCardLike(item);
   };
 
+  // Check if the item was liked by the current user
+  // The likes array should be an array of ids
+  const likes = {};
+  const isLiked = item.likes.some((id) => id === currentUser._id);
+
+  // Create a variable which you then set in `className` for the like button
+  const cardHeartClassName = `card__heart ${
+    isLiked ? "card__heart_visible" : "card__heart_hidden"
+  }`;
+
   return (
     <li className="card">
-      <h2 className="card__name">{item}</h2>
+      <h2 className="card__name">{item.name}</h2>
       {/* item.name */}
       <img
-        className="card__heart"
+        className={cardHeartClassName}
         onClick={handleLike}
         src={cardHeart}
         alt="card like button"
@@ -23,9 +33,9 @@ function ItemCard({ item, onCardClick, onCardLike }) {
       <img
         className="card__image"
         onClick={handleCardClick}
-        src={item}
+        src={item.imageUrl}
         // item.imageUrl
-        alt={item}
+        alt={item.name}
         // item.name
       />
     </li>
