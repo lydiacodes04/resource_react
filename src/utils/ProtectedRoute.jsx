5ea8b04d008e3d6aa52e8c9688e53fbea8131d56
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-02-22 15:23:30 -0600

    all

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -3,6 +3,7 @@ import { Navigate } from "react-router-dom";
 function ProtectedRoute({ isLoggedIn, children }) {
   if (!isLoggedIn) {
     // If user isn't logged in, return a Navigate component that sends the user to /login
+
     return <Navigate to="/login" replace />;
   }
   // Otherwise, render the protected route's child component.
