a15f71bde11d0b4f8ea8ba6963d0bef88058a4bb
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-03-06 21:08:32 -0600

    update navigate to from /login to / which is main page

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ function ProtectedRoute({ isLoggedIn, children }) {
   if (!isLoggedIn) {
     // If user isn't logged in, return a Navigate component that sends the user to /login
 
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/" replace />;
   }
   // Otherwise, render the protected route's child component.
   return children;
