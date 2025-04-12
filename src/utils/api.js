91c4ab803915961bff25f061f839a83de4ce07ad
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-04-12 17:22:54 -0500

    add VM deployed backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwr4me.justlearning.net"
+    : "http://localhost:3001";
 
 const getHeaders = () => ({
   "Content-Type": "application/json",
