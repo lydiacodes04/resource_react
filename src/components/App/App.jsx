ce30fca82e563d39dad0dcdc7c6c2e73c5035cc2
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-05-04 17:00:01 -0500

    committed to commits

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -317,3 +317,4 @@ export default App;
 //committed to commits 5/1
 //first submit of Project 15; finished lessons for Sprint 15 and started lessons for Sprint 16. Plan to finish course by ene of May 2025.
 //committed to commits 5/3
+//committed to commits 5/4
